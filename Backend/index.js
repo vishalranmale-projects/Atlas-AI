dd6c284df676aa1ff9875a22fccf8bc3ad9d6bdf
Vishal Ranmale <vishalranmale57@gmail.com>
2026-09-08 11:34:07 +0530

    Connected Backend To An Cloud Databace

--- Backend/index.js
+++ Backend/index.js
@@ -4,13 +4,15 @@ const mongoose = require("mongoose");
 const AiModel = require("./utils/OpenAi");
 const threadModel = require("./models/thread");
 const chatModel = require("./models/chat");
-
+require("dotenv").config();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.listen(3000,()=>{
    console.log("Server is Listen On An Port 3000");
 })
-
+mongoose.connect(`${process.env.MongoDBURL}`).then(()=>{
+    console.log("Connected To An Databace");
+})
 async function test(){
   console.log( await AiModel("Thanks"));
 }
